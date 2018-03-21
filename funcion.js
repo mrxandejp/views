e7d36da621b4161a61c9739e7fde5abcd9e6b929
Alexandre Santos Marques <alexandre.marques@lavid.ufpb.br>
2018-03-20 22:47:32 -0300

    tratador

--- funcion.js
+++ funcion.js
@@ -1,4 +1,3 @@
-
 function funcion(func, x) {
     return nerdamer(func).buildFunction().call(undefined, x);
 }
@@ -15,17 +14,17 @@ function procesar(formulario) {
     var func = document.getElementsByName("func")[0].value;
     
     var err, x_1, x = parseFloat(formulario.x.value);
+
+    var table = document.getElementById('tabela-numValores');
+    var row;
+    table.innerHTML="<tr></tr><tr></tr>";
     do {
         //x_1 = x;
         var x_1 = x - funcion(func, x) / derivada(x);
         var e = Math.abs(x-x_1);
         x = x_1;
         err = Math.abs((x - x_1) / x);
-
-      var table = document.getElementById('tabela-numValores');
-      var row;
         
-      
         row = table.insertRow(i+1);
         row.innerHTML = '<td class="col-md-6">x<sub>' + i + '</sub></td><td class="col-md-6">' + x_1 + '</td>'; 
 
